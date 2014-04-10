8c6cf62c378fb2c77594698b759c93914dbb2c4a
cptnmrgn <cptnmrgn10@gmail.com>
2014-04-10 14:22:21 +1000

    applied parallax

--- js/_main.js
+++ js/_main.js
@@ -31,7 +31,11 @@
 	function adjustWindow(){
 		
 		// Init Skrollr
-		
+		//var s = skrollr.init();
+
+		var s = skrollr.init({
+    	forceHeight: false
+		});
 		
 		// Get window size
 	    winH = $window.height();
@@ -45,7 +49,7 @@
 	    $slide.height(winH);
 	    
 	    // Refresh Skrollr after resizing our sections
-	    
+	    s.refresh($('.homeSlide'));
 	    
 	}
 		
